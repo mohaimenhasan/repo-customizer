c6bbb2caa76d892de480134d71b85c293cd2ae54
Mohaimen Khan <mohaimenkhan@microsoft.com>
2025-02-03 23:08:20 -0800

    Refactor using directives and update class visibility
    Removed unused using directives and added necessary ones for Azure and Newtonsoft.Json. Moved System.Text directive. Changed AzureOpenAIService to public.

--- AzureOpenAIService.cs
+++ AzureOpenAIService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
+﻿using System.Text;
 using Azure.Core;
 using Azure.Identity;
 using Newtonsoft.Json;
